8c18c0d22f155dc60ee796e45b7138a65e23d893
Zefeng Zhang <zzhang107@dons.usfca.edu>
2019-08-03 17:22:44 -0700

    add nn project

--- NN05-first-neural-network-project/my_answers.py
+++ NN05-first-neural-network-project/my_answers.py
@@ -42,8 +42,10 @@ class NeuralNetwork(object):
         
         '''
         n_records = features.shape[0]
+
         delta_weights_i_h = np.zeros(self.weights_input_to_hidden.shape)
         delta_weights_h_o = np.zeros(self.weights_hidden_to_output.shape)
+        
         for X, y in zip(features, targets):
             
             final_outputs, hidden_outputs = self.forward_pass_train(X)  # Implement the forward pass function below
@@ -102,6 +104,7 @@ class NeuralNetwork(object):
         delta_weights_i_h += X[:, None] * hidden_error_term[None, :]
         # Weight step (hidden to output)
         delta_weights_h_o += output_error_term * hidden_outputs[:, None]
+
         return delta_weights_i_h, delta_weights_h_o
 
 
@@ -115,8 +118,8 @@ class NeuralNetwork(object):
             n_records: number of records
 
         '''
-        self.weights_hidden_to_output += self.lr*delta_weights_h_o / n_records # update hidden-to-output weights with gradient descent step
-        self.weights_input_to_hidden += self.lr*delta_weights_i_h / n_records
+        self.weights_hidden_to_output += self.lr * delta_weights_h_o / n_records # update hidden-to-output weights with gradient descent step
+        self.weights_input_to_hidden += self.lr * delta_weights_i_h / n_records
 
     def run(self, features):
         ''' Run a forward pass through the network with input features 
